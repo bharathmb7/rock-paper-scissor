bd32b5b448481034a968d0844200caebd7cba390
bharathmb7 <bmb583219@gmail.com>
2023-08-24 13:29:35 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,17 @@
+Rock paper scissor game
+
+
+## Building and running on localhost
+
+First install dependencies:
+
+```sh
+npm install
+```
+
+To run in hot module reloading mode:
+
+```sh
+npm start
+```
+
---
+++ src/App.js
@@ -0,0 +1,30 @@
+import React, { useState } from "react";
+import Header from "./components/Header";
+import Play from "./components/Play";
+import Game from "./components/Game";
+import Footer from "./components/Footer";
+import { Routes, Route } from "react-router-dom";
+
+function App() {
+  const [myChoice, setMyChoice] = useState("");
+  const [score, setScore] = useState(0);
+
+  return (
+    <>
+      <div className="container">
+        <Header score={score} />
+        <Routes>
+          <Route path="/" element={<Play setMyChoice={setMyChoice} />} />
+          <Route
+            path="/game"
+            element={
+              <Game myChoice={myChoice} score={score} setScore={setScore} />
+            }
+          />
+        </Routes>
+      </div>
+    </>
+  );
+}
+
+export default App;
